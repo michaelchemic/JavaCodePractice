6a2dffed92ffdc606caaedb920c5159624a692c3
Michael Chemic <jl15312407650@gmail.com>
2022-03-18 13:29:19 +0800

    fix

---
+++ src/CodePractice/ComputeArea/ComputerArea.java
@@ -0,0 +1,10 @@
+package CodePractice.ComputeArea;
+
+public class ComputerArea {
+    public static void main(String[] args) {
+        double radius = 20;
+        double area =  radius*radius*3.14159;
+
+        System.out.println("The area for the circle of radius" + radius+"is"+area);
+    }
+}
